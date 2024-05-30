ce85c327b6f84a68934b082b3c7eac6ee52e3152
0529 <doin7767@gmail.com>
2024-05-30 14:42:07 +0900

    portfolio

---
+++ src/main/java/com/example/movie/dto/CustomerJoinDTO.java
@@ -0,0 +1,23 @@
+package com.example.movie.dto;
+
+import com.example.movie.user.Customer;
+import lombok.Data;
+
+@Data
+public class CustomerJoinDTO {
+    private String cid;
+    private String cpw;
+    private String email;
+    private String grade;
+    private boolean del;
+    private boolean social;
+
+
+    public static CustomerJoinDTO fromCustomer(Customer customer) {
+        CustomerJoinDTO customerJoinDTO = new CustomerJoinDTO();
+        customerJoinDTO.setCid(customerJoinDTO.getCid());
+        customerJoinDTO.setGrade(customerJoinDTO.getGrade());
+        //customerJoinDTO.setName(customerJoinDTO.getName());
+        return customerJoinDTO;
+    }
+}
